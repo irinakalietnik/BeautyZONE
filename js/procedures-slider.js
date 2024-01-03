e9d11da5b8e442fc1dc9b9e924be2ffcab571ceb
irinakalietnik <152523851+irinakalietnik@users.noreply.github.com>
2024-01-03 15:36:30 +0200

    procedures section finished

---
+++ js/procedures-slider.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper(".swiper-container", {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 16,
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+    },
+
+    768: {
+      slidesPerView: 2,
+    },
+    1280: { slidesPerView: 3 },
+  },
+  pagination: {
+    el: ".pagination",
+    bulletClass: "pagination__button",
+    bulletActiveClass: "pagination__button--active",
+  },
+  navigation: {
+    nextEl: ".btn-procedures.next",
+    prevEl: ".btn-procedures.prev",
+  },
+});
