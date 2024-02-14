1f448b63609f196e9cb1c2cb51f85160f154ceb3
irinakalietnik <152523851+irinakalietnik@users.noreply.github.com>
2024-02-14 15:30:11 +0200

    finish schroll mobil

--- js/mobil-menu.js
+++ js/mobil-menu.js
@@ -1,13 +1,18 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuBtnOpen = document.querySelector(".menu-btn-open");
 const menuBtnClose = document.querySelector(".menu-btn-close");
+const closeItemmenu = document.querySelectorAll(".close");
 
 const toggleMenu = () => mobileMenu.classList.toggle("is-open");
-const disableScroll = () =>
-  document.body.classList.toggle("is-scroll-disabled");
+//const disableScroll = () =>
+  ////document.body.classList.toggle("is-scroll-disabled");
 
-menuBtnOpen.addEventListener("click", toggleMenu);
+//menuBtnOpen.addEventListener("click", disableScroll);
+//menuBtnClose.addEventListener("click", disableScroll);
+
 menuBtnClose.addEventListener("click", toggleMenu);
+menuBtnOpen.addEventListener("click", toggleMenu);
 
-menuBtnOpen.addEventListener("click", disableScroll);
-menuBtnClose.addEventListener("click", disableScroll);
+for (elem of closeItemmenu) {
+  elem.addEventListener("click", toggleMenu);
+}
